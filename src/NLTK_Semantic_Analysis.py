d6d703a3bd0536c80ec531815053a25e2dae05cc
Siddarth <sjnraja0209@gmail.com>
2021-09-22 23:28:10 +0800

    Safety backup

--- src/NLTK_Semantic_Analysis.py
+++ src/NLTK_Semantic_Analysis.py
@@ -7,36 +7,49 @@ import json
 import requests
 from datetime import date
 import pandas as pd
-import matplotlib as plt
+from matplotlib import pyplot as plt
 sia = SentimentIntensityAnalyzer()
 today = date.today()
+#Currentlty we can see that we can only get a maximum of 100 data points 
+#However, a new solution is that we extract the first 100 posts (later all the posts) of each day and then calculate a cummulative semantic value 
+#Then we can plot the trend of the change of semantics to determine if there is a correlation
 #To determine number of days from start, to last scan 
 startDate = date(2020,1,22)
-numberOfDays= today-startDate
+#for testing purposes we set the enddate to a much lower value
+endDate = today
+numberOfDays= endDate-startDate
 print(str(numberOfDays.days))
 numberofRequestsNeeded = numberOfDays.days%100 + ceil((numberOfDays.days-(numberOfDays.days%100)*100)/100)*1
 comments =[]
 # for testing time we shall just use 100 days of data
 for i in range(0,1):
     textDays = str(numberOfDays.days-100*i)
-    tempUrl = f"https://api.pushshift.io/reddit/search/comment/?q=science&subreddit=wallstreetbets&after="+textDays+"d&size=100"
+    tempUrl = f"https://api.pushshift.io/reddit/search/comment/?q=science&subreddit=wallstreetbets&after="+textDays+"d&size=1000"
     request=requests.get(tempUrl)
     json_response = request.json()
     tempComments = json_response['data']
     comments.append(tempComments)
-print(len(comments))
-print(sia.polarity_scores("Wow, Sid is great!"))
-print(sia.polarity_scores("This is not sucky"))
-print("Look above")
+# print(len(comments))
+# print(sia.polarity_scores("Wow, Sid is great!"))
+# print(sia.polarity_scores("This is not sucky"))
+# print("Look above")
 totalPolarity = 0.0
-df = pd.DataFrame(comments)
-# df.to_csv("src/RedditPosts.csv")
 print(comments[0][0].keys())
 count = 0
+compoundPolarity = []
+datePosting = []
 # for testing time we shall just use 100 days of data
 for j in range(0,1):
     for i in comments[j]:
-        totalPolarity+= sia.polarity_scores(comments[j][0]["body"])['compound'] 
+        totalPolarity+= sia.polarity_scores(comments[j][count]["body"])['compound'] 
+        datePosting.append(count)
+        print(comments[j][count]["body"])
+        compoundPolarity.append(sia.polarity_scores(comments[j][count]["body"])['compound'])
         count +=1
 print(totalPolarity/count)
+print(count)
+plt.plot(datePosting, compoundPolarity)
+print(datePosting)
+# plt.ylim(-0.3600, -0.3612)
+plt.show()
 #there is a key 'created_utc' which can be used to find out which date a post was posted    
--- src/NYSECollection.py
+++ src/NYSECollection.py
@@ -1,21 +1,5 @@
-import numpy as np
-import pandas as pd
-
-#Data Source
 import yfinance as yf
 from matplotlib import pyplot as plt
-#Data viz
-import plotly.graph_objs as go
-# Interval required 5 minutes
-#NVDA is the stock data for NVIDIA
-data = yf.download(tickers='NVDA',start='2020-01-22', interval='1d')
-#AMD is the stock data for AMD 
-#UBI is the stock price for Ubisoft 
-#ATVI is stock name for blizzard
-#Print data
-data
-data.to_csv('src/stock.csv')
-print(data)
-print(data['High'])
+data = yf.download(tickers='BTC-USD',start='2015-01-22', interval='1d')
 plt.plot(data['High'])
-plt.show()
+plt.show()  
